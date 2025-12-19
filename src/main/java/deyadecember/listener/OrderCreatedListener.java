760d9a2e006b70bfdfc996cec0f78f3169fec967
dariia.khaniuchenko <dariia.khaniuchenko@imperva.com>
2025-12-19 15:35:27 +0200

    added retry and DLT(dead letter topic)

--- src/main/java/deyadecember/listener/OrderCreatedListener.java
+++ src/main/java/deyadecember/listener/OrderCreatedListener.java
@@ -1,23 +1,74 @@
 package deyadecember.listener;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import deyadecember.dto.OrderCreatedEvent;
 import deyadecember.producer.PaymentProducer;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.RetryableTopic;
+import org.springframework.kafka.support.Acknowledgment;
+import org.springframework.retry.annotation.Backoff;
 import org.springframework.stereotype.Service;
+
 
 @Service
 @RequiredArgsConstructor
 public class OrderCreatedListener {
 
     private final PaymentProducer producer;
-
+    private ObjectMapper objectMapper = new ObjectMapper();
+    private final Logger log = org.slf4j.LoggerFactory.getLogger(OrderCreatedListener.class);
 
     @KafkaListener(topics = "orders.created")
-    public void listen(OrderCreatedEvent event) {
+    @RetryableTopic(
+            attempts = "3",
+            backoff = @Backoff(delay = 2000),
+            dltTopicSuffix = ".dlq"
+    )
+    public void listen(String payload, Acknowledgment ack) throws JsonProcessingException {
 
-        System.out.println("💰 PaymentService received event: " + event);
-        producer.send( event);
+        try {
+            OrderCreatedEvent event = objectMapper.readValue(payload, OrderCreatedEvent.class);
+            log.info("Received order {}", event.getOrderId());
+
+            if (event.getAmount() <= 0) {
+                throw new IllegalArgumentException("Amount cannot be negative");
+
+            }
+            // 🧠 бізнес-логіка
+            //        producer.send( event);
+            log.info("Processing payment for order {}", event.getOrderId());
+
+            ack.acknowledge();
+
+        } catch (IllegalArgumentException e) {
+            log.error("Invalid event {}", payload, e);
+            throw e;
+
+        } catch (JsonProcessingException e) {
+            log.error("Invalid json {}", payload, e);
+            throw e;
+
+        } catch (Exception e) {
+            log.error("Temporary error", e);
+            throw e;
+        }
     }
+
+    @KafkaListener(
+            topics = "orders.created.dlq",
+            groupId = "payment-service-dlq"
+    )
+    public void handleDlq(String payload) {
+        try {
+            OrderCreatedEvent event = objectMapper.readValue(payload, OrderCreatedEvent.class);
+            log.error("Message in DLQ: {}", event);
+        } catch (JsonProcessingException e) {
+            log.error("Invalid json in DLQ: {}", payload, e);
+        }
+    }
+
 }
 
